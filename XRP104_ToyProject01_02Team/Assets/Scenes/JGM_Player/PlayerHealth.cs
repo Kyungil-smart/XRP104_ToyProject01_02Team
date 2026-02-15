0a1d0a29d9f4b8c54f078738b4abb42053b7e949
jogyemin <jogyemin5100@gmail.com>
2026-02-15 18:07:43 +0900

    JGM_PlayShotandDied
    플레이어 피격 및 사망 시스템 구현
    
    1.  플레이어 체력(HP) 관리
    2.  외부에서 데미지를 받을 수 있는 구조 설계
    3.  체력이 0 이하가 되면 사망 처리
    4.  사망 시 플레이어 조작 차단
    5.  GameManager와 연결 가능한 구조 설계 (추후 구현)

---
+++ XRP104_ToyProject01_02Team/Assets/Scenes/JGM_Player/PlayerHealth.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [Header("Health Settings")]
+    [SerializeField] private float maxHP = 100f;
+
+    private float _currentHP;
+    private bool _isDead = false;
+    private PlayerMovement _movement;
+
+    private void Awake()
+    {
+        _currentHP = maxHP;
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (_isDead)
+            return;
+
+        if (damage <= 0f)
+            return;
+
+        _currentHP -= damage;
+
+        Debug.Log($"플레이어 피격: {damage} / 남은 HP: {_currentHP}");
+
+        if (_currentHP <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (_isDead)
+            return;
+
+        _isDead = true;
+
+        Debug.Log("플레이어 사망");
+
+        if (_movement != null)
+        {
+            _movement.enabled = false;
+        }
+
+        // TODO: 사망 애니메이션 추가 예정
+
+        // TODO: GameManager 구현 후 연결 예정
+        // GameManager.Instance.GameOver();
+    }
+
+    public float GetCurrentHP()
+    {
+        return _currentHP;
+    }
+
+    public bool IsDead()
+    {
+        return _isDead;
+    }
+}
