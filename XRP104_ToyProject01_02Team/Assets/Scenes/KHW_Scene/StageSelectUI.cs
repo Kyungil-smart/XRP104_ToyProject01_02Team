85480c1814c2ad346f6afce31dd8a1f714f0b823
khw2176 <khw2176@gmail.com>
2026-02-17 15:00:20 +0900

    Clear, Pause, Over

--- XRP104_ToyProject01_02Team/Assets/Scenes/KHW_Scene/StageSelectUI.cs
+++ XRP104_ToyProject01_02Team/Assets/Scenes/KHW_Scene/StageSelectUI.cs
@@ -3,14 +3,54 @@ using UnityEngine.SceneManagement;
 
 public class StageSelectUI : MonoBehaviour
 {
-    public void SelectStage(int stage)
+    public GameObject pausePopup;
+    public GameObject gameOverPopup;
+    public GameObject stageClearPopup;
+
+    bool isPaused = false;
+
+    private void Update()
     {
-        StageManager.SelectedStage = stage;
-        SceneManager.LoadScene(2);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
     }
 
-    public void BackToTitle()
+    // Pause On/Off
+    public void TogglePause()
     {
+        isPaused = !isPaused;
+        pausePopup.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    // Continue 버튼
+    public void ContinueGame()
+    {
+        isPaused = false;
+        pausePopup.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Title 이동
+    public void GoToTitle()
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    // Game Over
+    public void ShowGameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverPopup.SetActive(true);
+    }
+
+    // Stage Clear
+    public void ShowStageClear()
+    {
+        Time.timeScale = 0f;
+        stageClearPopup.SetActive(true);
     }
 }
